6636c646a98ac309a81139513e105938b950bd0e
tkmagesh <tkmagesh77@gmail.com>
2014-03-28 13:05:24 +0530

    Closures Demo

--- Day-1/calculator.js
+++ Day-1/calculator.js
@@ -1,3 +1,8 @@
 function sum(){
-	
+	function parseArg(n){
+		if (typeof n === "function") return parseArg(n());
+		if (Array.isArray(n)) return sum.apply(this,n);
+		return (isNaN(n)) ? 0 : parseInt(n);
+	}
+	return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + sum([].slice.call(arguments,1));
 }

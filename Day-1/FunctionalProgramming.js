5d4f2f822fa82ba50e4a70593f9725e939902218
tkmagesh <tkmagesh77@gmail.com>
2014-03-28 16:22:42 +0530

    Functional Programming Techniques

---
+++ Day-1/FunctionalProgramming.js
@@ -0,0 +1,13 @@
+var products = [
+	{id : 101, name:"pen", cost:10, units :20, category:1},
+	{id : 107, name:"hen", cost:21, units :21, category:2},
+	{id : 102, name:"ten", cost:31, units :29, category:1},
+	{id : 106, name:"den", cost:18, units :22, category:2},
+	{id : 104, name:"len", cost:15, units :25, category:1},
+	{id : 109, name:"zen", cost:61, units :23, category:2},
+]
+
+/*
+1. write a sort function that will by defaul sort  the products by id
+2. modify the sort function in such a way that the function can be used to sort the products by any attribute
+*/

000d70c37e738a47484b67bc2400a833a0aed65e
tkmagesh <tkmagesh77@gmail.com>
2014-03-29 14:18:54 +0530

    Node Demo - 2

--- Day-2/02.Node/firstWebServer/firstWebServer.js
+++ Day-2/02.Node/firstWebServer/firstWebServer.js
@@ -1,5 +1,35 @@
-require("http").createServer(function(req,res){
-	res.write("<h1>Welcome to Node</h1>");
-	res.end();
+var http = require("http")
+ 	,url = require("url")
+	,fs = require("fs");
+
+http.createServer(function(req,res){
+	var fileName = "." + url.parse(req.url).pathname;
+	if (!fs.existsSync(fileName)){
+		res.statusCode = 404;
+		res.end();
+	} else {
+		var stream = fs.createReadStream(fileName, {encoding : "utf8"});
+		/*var dataEventCount = 0;
+		stream.on("data",function(dataChunk){
+			++dataEventCount;
+			res.write(dataChunk);
+		});
+		stream.on("end",function(){
+			console.log("dataEventCount = ", dataEventCount);
+			res.end();
+		});*/
+		stream.pipe(res);
+		
+		/*
+		fs.readFile(fileName,function(err,data){
+			if (err){
+				res.statusCode = 500;
+			} else {
+				res.write(data);
+			}
+			res.end();
+		});
+		*/
+	}
 }).listen(9090);
 console.log("Server listening on port 9090");
